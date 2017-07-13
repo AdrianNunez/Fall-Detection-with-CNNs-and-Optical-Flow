a1bd3840e7ddbc1e2d3b9ce828fccff08477466e
AdrianNunez <adrianwakefiss@gmail.com>
2017-07-13 12:51:19 +0200

    temporalnetfdd.py fixed

--- temporalnetfdd.py
+++ temporalnetfdd.py
@@ -10,7 +10,6 @@ import os
 from PIL import Image
 import io
 from sklearn.model_selection import StratifiedShuffleSplit
-from vgg16module import VGG16
 
 from keras.models import Model, model_from_json, model_from_yaml, Sequential
 from keras.layers import Input, Convolution2D, MaxPooling2D, LSTM, Reshape, Merge, TimeDistributed, Flatten, Activation, Dense, Dropout, merge, AveragePooling2D, ZeroPadding2D, Lambda
@@ -83,90 +82,94 @@ def generator(folder1, folder2):
         yield x, y
           
 def saveFeatures(feature_extractor, features_file, labels_file):
-        data_folder = '/ssd_drive/FDD_Fall_OF/'
-        mean_file = '/ssd_drive/flow_mean.mat'
-        L = 10 
-        
-        class0 = 'Falls'
-        class1 = 'NotFalls'
+    data_folder = '/ssd_drive/FDD_Fall_OF/'
+    mean_file = '/ssd_drive/flow_mean.mat'
+    L = 10 
+    
+    class0 = 'Falls'
+    class1 = 'NotFalls'
 
-        d = sio.loadmat(mean_file)
-        flow_mean = d['image_mean']
-        num_features = 4096
-      
-        folders, classes = [], []
-        fall_videos = [f for f in os.listdir(data_folder + class0) if os.path.isdir(os.path.join(data_folder + class0, f))]
-        fall_videos.sort()
-        for fall_video in fall_videos:
-            x_images = glob.glob(data_folder + class0 + '/' + fall_video + '/flow_x*.jpg')
-            if int(len(x_images)) >= 10:
-                folders.append(data_folder + class0 + '/' + fall_video)
-                classes.append(0)
+    d = sio.loadmat(mean_file)
+    flow_mean = d['image_mean']
+    num_features = 4096
+  
+    folders, classes = [], []
+    fall_videos = [f for f in os.listdir(data_folder + class0) if os.path.isdir(os.path.join(data_folder + class0, f))]
+    fall_videos.sort()
+    for fall_video in fall_videos:
+        x_images = glob.glob(data_folder + class0 + '/' + fall_video + '/flow_x*.jpg')
+        if int(len(x_images)) >= 10:
+            folders.append(data_folder + class0 + '/' + fall_video)
+            classes.append(0)
 
-        not_fall_videos = [f for f in os.listdir(data_folder + class1) if os.path.isdir(os.path.join(data_folder + class1, f))]
-        not_fall_videos.sort()
-        for not_fall_video in not_fall_videos:
-            x_images = glob.glob(data_folder + class1 + '/' + not_fall_video + '/flow_x*.jpg')
-            if int(len(x_images)) >= 10:
-                folders.append(data_folder + class1 + '/' + not_fall_video)
-                classes.append(1)
+    not_fall_videos = [f for f in os.listdir(data_folder + class1) if os.path.isdir(os.path.join(data_folder + class1, f))]
+    not_fall_videos.sort()
+    for not_fall_video in not_fall_videos:
+        x_images = glob.glob(data_folder + class1 + '/' + not_fall_video + '/flow_x*.jpg')
+        if int(len(x_images)) >= 10:
+            folders.append(data_folder + class1 + '/' + not_fall_video)
+            classes.append(1)
 
-        h5features = h5py.File(features_file,'w')
-        h5labels = h5py.File(labels_file,'w')
-       
-        nb_total_stacks = 0
-        for folder in folders:
-            x_images = glob.glob(folder + '/flow_x*.jpg')
-            nb_total_stacks += int(len(x_images))-L+1
-               
-        X = folders
-        y = classes
-        dataset_features = h5features.create_dataset('features', shape=(nb_total_stacks, num_features), dtype='float64')
-        dataset_labels = h5labels.create_dataset('labels', shape=(nb_total_stacks, 1), dtype='float64')  
-      
-        cont = 0
+    h5features = h5py.File(features_file,'w')
+    h5labels = h5py.File(labels_file,'w')
+   
+    nb_total_stacks = 0
+    for folder in folders:
+        x_images = glob.glob(folder + '/flow_x*.jpg')
+        nb_total_stacks += int(len(x_images))-L+1
+           
+    X = folders
+    y = classes
+    dataset_features = h5features.create_dataset('features', shape=(nb_total_stacks, num_features), dtype='float64')
+    dataset_labels = h5labels.create_dataset('labels', shape=(nb_total_stacks, 1), dtype='float64')    
+    cont = 0
 
-	for folder, label in zip(X, y):
-	    x_images = glob.glob(folder + '/flow_x*.jpg')
-	    x_images.sort()
-	    y_images = glob.glob(folder + '/flow_y*.jpg')
-	    y_images.sort()
-	    nb_stacks = int(len(x_images))-L+1
-	    flow = np.zeros(shape=(224,224,2*L,nb_stacks), dtype=np.float64)
-	    gen = generator(x_images,y_images)
-	    for i in range(len(x_images)):
-		flow_x_file, flow_y_file = gen.next()
-		img_x = cv2.imread(flow_x_file, cv2.IMREAD_GRAYSCALE)
-		img_y = cv2.imread(flow_y_file, cv2.IMREAD_GRAYSCALE)
-		for s in list(reversed(range(min(10,i+1)))):
-		    if i-s < nb_stacks:
-			flow[:,:,2*s,  i-s] = img_x
-			flow[:,:,2*s+1,i-s] = img_y
-		del img_x,img_y
-		gc.collect()
-	    flow = flow - np.tile(flow_mean[...,np.newaxis], (1, 1, 1, flow.shape[3]))
-	    flow = np.transpose(flow, (3, 2, 0, 1)) 
-	    predictions = np.zeros((flow.shape[0], num_features), dtype=np.float64)
-	    truth = np.zeros((flow.shape[0], 1), dtype=np.float64)
-	    for i in range(flow.shape[0]):
-		prediction = feature_extractor.predict(np.expand_dims(flow[i, ...],0))
-		predictions[i, ...] = prediction
-		truth[i] = label
-	    dataset_features[cont:cont+flow.shape[0],:] = predictions
-	    dataset_labels[cont:cont+flow.shape[0],:] = truth
-	    cont += flow.shape[0]
-        h5features.close()
-        h5labels.close()
+    for folder, label in zip(X, y):
+        x_images = glob.glob(folder + '/flow_x*.jpg')
+        x_images.sort()
+        y_images = glob.glob(folder + '/flow_y*.jpg')
+        y_images.sort()
+        nb_stacks = int(len(x_images))-L+1
+        flow = np.zeros(shape=(224,224,2*L,nb_stacks), dtype=np.float64)
+        gen = generator(x_images,y_images)
+        for i in range(len(x_images)):
+            flow_x_file, flow_y_file = gen.next()
+            img_x = cv2.imread(flow_x_file, cv2.IMREAD_GRAYSCALE)
+            img_y = cv2.imread(flow_y_file, cv2.IMREAD_GRAYSCALE)
+            for s in list(reversed(range(min(10,i+1)))):
+                if i-s < nb_stacks:
+                    flow[:,:,2*s,  i-s] = img_x
+                    flow[:,:,2*s+1,i-s] = img_y
+            del img_x,img_y
+        gc.collect()
+        flow = flow - np.tile(flow_mean[...,np.newaxis], (1, 1, 1, flow.shape[3]))
+        flow = np.transpose(flow, (3, 2, 0, 1)) 
+        predictions = np.zeros((flow.shape[0], num_features), dtype=np.float64)
+        truth = np.zeros((flow.shape[0], 1), dtype=np.float64)
+        for i in range(flow.shape[0]):
+            prediction = feature_extractor.predict(np.expand_dims(flow[i, ...],0))
+            predictions[i, ...] = prediction
+            truth[i] = label
+        dataset_features[cont:cont+flow.shape[0],:] = predictions
+        dataset_labels[cont:cont+flow.shape[0],:] = truth
+        cont += flow.shape[0]
+    h5features.close()
+    h5labels.close()
 
 def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, weights_file): 
     exp = 'lr{}_batchs{}_batchnorm{}_w0_{}'.format(learning_rate, mini_batch_size, batch_norm, w0)
-    best_model = 'best_weights/best_weights_{}.hdf5'.format(exp)
+    best_model = 'best_weights_{}.hdf5'.format(exp)
+    vgg_16_weights = 'weights.h5'
     balance_dataset = True
     save_plots = True
     num_features = 4096
     features_file = 'features_fdd.h5'
     labels_file = 'labels_fdd.h5'
     save_features = False
+
+    # =============================================================================================================
+    # VGG-16 ARCHITECTURE
+    # =============================================================================================================
          
     model = Sequential()
     
@@ -208,13 +211,13 @@ def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, we
     
     model.add(Flatten())
     model.add(Dense(4096, name='fc6', init='glorot_uniform'))
-   
-    
 
-    layerskeras = ['block1_conv1', 'block1_conv2', 'block2_conv1', 'block2_conv2', 'block3_conv1', 'block3_conv2', 'block3_conv3', 'block4_conv1', 'block4_conv2', 'block4_conv3', 'block5_conv1', 'block5_conv2', 'block5_conv3', 'fc1', 'fc2', 'predictions']
+    # =============================================================================================================
+    # WEIGHT INITIALIZATION
+    # =============================================================================================================
+   
     layerscaffe = ['conv1_1', 'conv1_2', 'conv2_1', 'conv2_2', 'conv3_1', 'conv3_2', 'conv3_3', 'conv4_1', 'conv4_2', 'conv4_3', 'conv5_1', 'conv5_2', 'conv5_3', 'fc6', 'fc7', 'fc8']
-    i = 0
-    h5 = h5py.File('/home/anunez/project/caffedata.h5')
+    h5 = h5py.File(vgg_16_weights)
     
     layer_dict = dict([(layer.name, layer) for layer in model.layers])
 
@@ -225,7 +228,6 @@ def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, we
         b2 = np.asarray(b2)
         layer_dict[layer].W.set_value(w2)
         layer_dict[layer].b.set_value(b2)
-        i += 1
         
     layer = layerscaffe[-3]
     w2, b2 = h5['data'][layer]['0'], h5['data'][layer]['1']
@@ -233,25 +235,13 @@ def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, we
     b2 = np.asarray(b2)
     layer_dict[layer].W.set_value(w2)
     layer_dict[layer].b.set_value(b2)
-    i += 1
-    
-    copy_dense_weights = False
-    if copy_dense_weights:
-        layer = layerscaffe[-2]
-        w2, b2 = h5['data'][layer]['0'], h5['data'][layer]['1']      
-        w2 = np.transpose(w2,(1,0))
-        b2 = np.asarray(b2)
-        i += 1
 
     adam = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0005)
     model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy'])
-    c = ModelCheckpoint(filepath=best_model, monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto')
-    e = EarlyStopping(monitor='loss', min_delta=0, patience=0, verbose=0, mode='auto')
 
     # =============================================================================================================
     # FEATURE EXTRACTION
-    # =============================================================================================================
-    
+    # ============================================================================================================= 
     if save_features:
         saveFeatures(model, features_file, labels_file)
 
@@ -296,8 +286,7 @@ def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, we
             X = np.concatenate((X_full[train_index_falls, ...], X_full[train_index_nofalls, ...]))
             _y = np.concatenate((_y_full[train_index_falls, ...], _y_full[train_index_nofalls, ...]))
             X2 = np.concatenate((X_full[test_index_falls, ...], X_full[test_index_nofalls, ...]))
-            _y2 = np.concatenate((_y_full[test_index_falls, ...], _y_full[test_index_nofalls, ...]))
-        
+            _y2 = np.concatenate((_y_full[test_index_falls, ...], _y_full[test_index_nofalls, ...])) 
          
             all0 = np.asarray(np.where(_y==0)[0])
             all1 = np.asarray(np.where(_y==1)[0])   
@@ -316,16 +305,16 @@ def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, we
             if batch_norm:
                 x = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(extracted_features)
                 x = ELU(alpha=1.0)(x)
-	    else:
+            else:
             	x = ELU(alpha=1.0)(extracted_features)
 	    
             x = Dropout(0.9)(x)
             x = Dense(4096, name='fc2', init='glorot_uniform')(x)
             if batch_norm:
                 x = BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001)(x)
-		x = Activation('relu')(x)
+                x = Activation('relu')(x)
             else:
-		x = ELU(alpha=1.0)(x)
+                x = ELU(alpha=1.0)(x)
 
             x = Dropout(0.8)(x)
             x = Dense(1, name='predictions', init='glorot_uniform')(x)
@@ -333,21 +322,20 @@ def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, we
 
             classifier = Model(input=extracted_features, output=x, name='classifier')
             classifier.compile(optimizer=adam, loss='binary_crossentropy',  metrics=['accuracy'])
-	    # ==================== CLASSIFIER ========================
-           
+
+            # ==================== TRAINING ========================         
             class_weight = {0:weight_0, 1:1}
             if batch_size == 0:
             	history = classifier.fit(X,_y, validation_data=(X2,_y2), batch_size=X.shape[0], nb_epoch=epochs, shuffle='batch', class_weight=class_weight)
-	    else:
-		history = classifier.fit(X,_y, validation_data=(X2,_y2), batch_size=batch_size, nb_epoch=epochs, shuffle='batch', class_weight=class_weight)
-            plot_training_info('prueba', ['accuracy', 'loss'], save_plots, history.history)
+            else:
+                history = classifier.fit(X,_y, validation_data=(X2,_y2), batch_size=batch_size, nb_epoch=epochs, shuffle='batch', class_weight=class_weight)
+            plot_training_info(exp, ['accuracy', 'loss'], save_plots, history.history)
 
+            # ==================== EVALUATION ========================        
             if compute_metrics:
                threshold = 0.5
                predicted = classifier.predict(np.asarray(X2))
-               ind0 = np.where(np.asarray(_y2)<threshold)[0]
-               ind1 = np.where(np.asarray(_y2)>=threshold)[0]
-	       for i in range(len(predicted)):
+               for i in range(len(predicted)):
                    if predicted[i] < threshold:
                		predicted[i] = 0
                    else:
@@ -362,26 +350,26 @@ def main(learning_rate, batch_size, batch_norm, weight_0, epochs, model_file, we
                fpr = fp/float(fp+tn)
                fnr = fn/float(fn+tp)
                tnr = tn/float(tn+fp)
-	       precision = tp/float(tp+fp)
+               precision = tp/float(tp+fp)
                recall = tp/float(tp+fn)
-	       specificity = tn/float(tn+fp)
-	       f1 = 2*float(precision*recall)/float(precision+recall)
-	       accuracy = accuracy_score(_y2, predicted)
+               specificity = tn/float(tn+fp)
+               f1 = 2*float(precision*recall)/float(precision+recall)
+               accuracy = accuracy_score(_y2, predicted)
                print('TP: {}, TN: {}, FP: {}, FN: {}'.format(tp,tn,fp,fn))
                print('TPR: {}, TNR: {}, FPR: {}, FNR: {}'.format(tpr,tnr,fpr,fnr))   
                print('Sensitivity/Recall: {}'.format(recall))
                print('Specificity: {}'.format(specificity))
                print('Precision: {}'.format(precision))
                print('F1-measure: {}'.format(f1))
-	       print('Accuracy: {}'.format(accuracy))
+               print('Accuracy: {}'.format(accuracy))
                sensitivities.append(tp/float(tp+fn))
                specificities.append(tn/float(tn+fp))
                accuracies.append(accuracy)
+               
         print('5-FOLD CROSS-VALIDATION RESULTS ===================')
         print("Sensitivity: %.2f%% (+/- %.2f%%)" % (np.mean(sensitivities), np.std(sensitivities)))
         print("Specificity: %.2f%% (+/- %.2f%%)" % (np.mean(specificities), np.std(specificities)))
         print("Accuracy: %.2f%% (+/- %.2f%%)" % (np.mean(accuracies), np.std(accuracies)))
-        print(exp)             
         
 if __name__ == '__main__':
     model_file = '/home/anunez/project/models/exp_'
