50bb2c53452a1449bc6488cfc0f236d9a7ced455
AdrianNunez <adrianwakefiss@gmail.com>
2018-01-27 20:46:31 +0100

    README file updated and brightness.py commented

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Fall-Detection-with-CNNs-and-Optical-Flow
+## Fall-Detection-with-CNNs-and-Optical-Flow
 
 This repository contains the code for our paper:
 
@@ -24,12 +24,38 @@ If you find the code useful for your research, please, cite our paper:
 
 We provide the following material to replicate the experiments.
 
-## Necessary files
+### Getting started
 
-* [Weights of the VGG16 network](https://drive.google.com/file/d/0B4i3D0pfGJjYNWxYTVUtNGtRcUE/view?usp=sharing)
-* [Mean file](https://drive.google.com/file/d/0B4i3D0pfGJjYTllxc0d2NGUyc28/view?usp=sharing)
+The repository contains the following files:
 
-## Extracted features and labels
+* **temporalnetxxx.py**: there are four scripts with this name, three of those are specific to a dataset (URFD, FDD and Multicam) and the remaining one, **temporalnetgeneral.py** it is the code for the experiment shown in the section 4.6 of the paper (a multi-tasking training with the previous three datasets). The four scripts do a feed-forward pass through the VGG16 of the optical flow stacks generated from the images of those datasets to get their features (downloadable below). Then, they fine-tune three fully-connected layers with those features to correctly classify actions.
+
+* **vgg16module.py** contains the VGG16 architecture in Keras.
+
+* **brightness.py** contains the script to generate the images used in the experiment of section 4.5 of the paper.
+
+### Reproducing the experiments
+
+Necessary files:
+
+* [Weights of the VGG16 network](https://drive.google.com/file/d/0B4i3D0pfGJjYNWxYTVUtNGtRcUE/view?usp=sharing). Weights of a pre-trained VGG16 in the UCF101 Action Recognition dataset using optical flow stacks.
+* [Mean file](https://drive.google.com/file/d/0B4i3D0pfGJjYTllxc0d2NGUyc28/view?usp=sharing).
+
+#### 0. Using your own dataset
+
+If you want to use your own dataset you need to have a directory with two subfolders: one called 'Falls' and the other one 'NotFalls'. Inside each of them there should be a folder for each fall/"not fall" video, where the images of the video are stored.
+
+#### x. Reproducing the experiment with different lighting conditions (Section 4.5 of the paper)
+
+**brightness.py** is required to darken or adding a lighting change to the original images of any dataset. 
+
+1. Change the variables 'data_folder' and 'output_path' at the top of the script to match with the path to your dataset and the outputh path you desire.
+
+2. The 'mode' variable can have the value 'darken' to obtain the images for the experiment 4.5.1 (darkened images). Any other value will retrieve the images for the experiment 4.5.2 (lighting change).
+
+3. You can also change the amount of darkness is added and the intensity of the added dynamic light applied. The variables 'darkness' and 'brightness' will control them, respectively.
+
+### Extracted features and labels
 
 These are the features extracted from the fc6 layer of the VGG16 network used for fine-tuning (vectors of size 4096). The instances are ordered so that the i-th element in the feature file has the i-th label of the labels file. For the Multicam dataset, the division inside each file is done by scenes, then by cameras.
 
--- brightness.py
+++ brightness.py
@@ -3,52 +3,75 @@ import os
 import sys
 import numpy as np
 
-data_folder = '/home/adrian/project/FALL/FDD_Fall/'
-output_path = '/home/adrian/project/FALL/FDD_Fall_Dynamic_2/'
+## VARIABLES TO CHANGE
+data_folder = 'FDD_Fall/'
+output_path = 'FDD_Fall_Dynamic/'
+mode = 'darken'
 
 if not os.path.exists(output_path):
     os.makedirs(output_path)
 
-folders = [f for f in os.listdir(data_folder + 'Falls') if os.path.isdir(os.path.join(data_folder + 'Falls', f))]
-brightness = 50 # 50, 100
-step = 0.1
+# EXPERIMENT 4.5.1: Matrix to be subtracted to the images to make them darker
+darkness = 100
+darkness_add = np.zeros((224,224,3))
+darkness_add.fill(brightness)
 
+# EXPERIMENT 4.5.2: Lighting change, the sin function is applied to the variable n, step is added to n after each frame
+# n_start is the starting value of n
+# Therefore, n=n_start in the frame 0, n=n_start+step in frame 1, n=n_start+2*step in frame 2 and so on
+# The final transformation is the matrix brightness*sin(n) added to the original image
+brightness = 50
 brightness_add = np.zeros((224,224,3))
 brightness_add.fill(brightness)
-n = 0
 
+step = 0.1
+n_start = 0
+
+# Process the folders inside the 'Falls' directory
+folders = [f for f in os.listdir(data_folder + 'Falls') if os.path.isdir(os.path.join(data_folder + 'Falls', f))]
 for folder in folders:
-    print('=========== {}'.format(folder))
     images = [f for f in os.listdir(data_folder + 'Falls/' + folder) if os.path.isfile(os.path.join(data_folder + 'Falls/' + folder, f))]
     images.sort()
-    n = 0
-    t = True
+    n = n_start
+    stop = False
+
+    # Process all the images of a video
     for image, nb_image in zip(images, range(len(images))):
         x = cv2.imread(data_folder + 'Falls/' + folder + '/' + image)
-        if brightness*np.sin(n)<0:
-            t = False
-        if t:
-            x = np.add(x, brightness_add*np.sin(n))
-            n += step
+	if mode == 'darken':
+	    x = np.add(x, darkness_add)
+	else:
+	    # if sin(n) is below 0 (the light is switched off) remove it
+	    if brightness*np.sin(n) < 0:
+	        stop = True
+	    if stop:
+	        x = np.add(x, brightness_add*np.sin(n))
+	        n += step
         if not os.path.exists(output_path + 'Falls/' + folder):
             os.makedirs(output_path + 'Falls/' + folder)
         cv2.imwrite(output_path + 'Falls/' + folder + '/' + image, x, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
-                
-folders = [f for f in os.listdir(data_folder + 'NotFalls') if os.path.isdir(os.path.join(data_folder + 'NotFalls', f))]
 
+# Process the folders inside the 'NotFalls' directory          
+folders = [f for f in os.listdir(data_folder + 'NotFalls') if os.path.isdir(os.path.join(data_folder + 'NotFalls', f))]
 for folder in folders:
-    print('=========== {}'.format(folder))
     images = [f for f in os.listdir(data_folder + 'NotFalls/' + folder) if os.path.isfile(os.path.join(data_folder + 'NotFalls/' + folder, f))]
     images.sort()
     n = 0
-    t = True
+    stop = True
+    # Process all the images of a video
     for image, nb_image in zip(images, range(len(images))):
-        x = cv2.imread(data_folder + 'NotFalls/' + folder + '/' + image) 
-        if brightness*np.sin(n)<0:
-            t = False
-        if t:
-            x = np.add(x, brightness_add*np.sin(n))
-            n += step
+        x = cv2.imread(data_folder + 'NotFalls/' + folder + '/' + image)      
+	if mode == 'darken':
+	    x = np.add(x, darkness_add)
+	else:
+	    # if sin(n) is below 0 (the light is switched off) remove it
+	    if brightness*np.sin(n) < 0:
+	        stop = True
+	    if stop:
+	        x = np.add(x, brightness_add*np.sin(n))
+	        n += step
         if not os.path.exists(output_path + 'NotFalls/' + folder):
             os.makedirs(output_path + 'NotFalls/' + folder)
         cv2.imwrite(output_path + 'NotFalls/' + folder + '/' + image, x, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+
+print('=== END ===')
